62dc154a8fed35b5e07b730998e2bd8f5f31f0e0
zhoucongmin <1641626308@qq.com>
2016-05-06 14:54:26 +0800

    react 案例上传1

---
+++ react案例/amazeui-react使用案例/build/main.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var AMUIReact = require('amazeui-react');
+//////////////////////////////////////////////////
+var modalInstance=require('./js/ama.js');
+ReactDOM.render(modalInstance, document.querySelector('.box'));
+
